10ff6bda5611508cd516dbded5dc9bfe254046a7
unknown <=>
2018-01-14 19:05:13 +0100

    version14/01/2018

--- wp-content/themes/portfolio/template-parts/content.php
+++ wp-content/themes/portfolio/template-parts/content.php
@@ -11,15 +11,17 @@
 
 <article class="vr-post-content">
 	<header>
-		<img src="<?= get_the_post_thumbnail_url('');?>">
+		<img class="vr-photo-post" src="<?= get_the_post_thumbnail_url('');?>">
+		<div class="vr-title-post">
 		<?php
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
 		endif;
 
 		if ( 'post' === get_post_type() ) : ?>
+	</div>
 		<div>
 			<?php //portfolio_posted_on(); ?>
 		</div>
@@ -31,11 +33,12 @@
 
 	<footer>
 		<?php if( get_field('ajouter_un_pdf') ): ?>
-
+			<div>
 	<a href="<?php the_field('ajouter_un_pdf'); ?>">
 		<img class="vr-pdf"src="<?= get_template_directory_uri()?>/assets/images/pdf.svg">
-		<span>Download File</span>
+		<span>Télécharger</span>
 	</a>
+	</div>
 
 <?php endif; ?>
 
